02be20a4aa0626135da7b8d6cf96e549dad3252c
deepdeep5308 <111973793+deepdeep5308@users.noreply.github.com>
2023-10-03 03:12:30 +0000

    DataTypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,15 @@
+const accountId = 1323
+let accountEmail = "deep@google.com"
+var accountPassword = "123432"
+accountCity = "Delhi"
+let accountState;
+
+// accountId = 2 
+// not allowed in JS
+accountPassword = "avjcj"
+accountCity = "Jaipur"
+// console.log(accountEmail)
+
+// console.log(accountPassword)
+
+console.table([accountCity,accountEmail,accountId,accountPassword,accountState])
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,8 @@
+let score = "Deepak"
+
+console.log(score);
+console.log(typeof score);
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
