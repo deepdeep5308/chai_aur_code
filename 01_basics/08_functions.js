0aa320136170bb9a8e300531000cd2a38132a9f0
deepdeep5308 <111973793+deepdeep5308@users.noreply.github.com>
2023-10-30 13:41:25 +0000

    functions

--- 01_basics/08_functions.js
+++ 01_basics/08_functions.js
@@ -1,5 +1,30 @@
-function userLogin (username){
-    return `${username},just log in`
+// function userLogin (username){
+//     if(username===undefined){
+//         console.log('Please enter your name')
+//     }
+//     else{
+//     return `${username},just log in`}
+// }
+
+// console.log(userLogin())
+
+function cartPrice(val1,val2,...num1){
+    return num1
+}
+// console.log(cartPrice(200,400,2000))
+
+const user = {
+    username:"Deep",
+    price: 199
+}
+function forObject(anyobject){
+    console.log(`Username is ${anyobject.username} and price is ${anyobject.price}`)
 }
 
-console.log(userLogin('Deep'))
+forObject(user)
+
+forObject({
+    username: "Sammy",
+    price: 499
+})
+
